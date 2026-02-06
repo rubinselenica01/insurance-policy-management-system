2775ff41b695ca601e956fcedc4d4639cb93330d
rubinselenica01 <selenicarubin@gmail.com>
2026-02-06 13:20:53 +0100

    Initial commit

---
+++ src/main/java/com/rubin/insurance/policy_management_service/InsurancePolicyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rubin.insurance.policy_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancePolicyManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancePolicyManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubin/insurance/policy_management_service/InsurancePolicyManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubin.insurance.policy_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancePolicyManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
