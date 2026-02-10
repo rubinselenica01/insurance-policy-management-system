3e6a10b2333e622a62467578772e52935f062d5a
rubinselenica01 <selenicarubin@gmail.com>
2026-02-10 14:08:31 +0100

    Add Event Types for each entity

---
+++ src/main/java/com/rubin/insurance/policy_management_service/events/ClaimEventType.java
@@ -0,0 +1,7 @@
+package com.rubin.insurance.policy_management_service.events;
+
+public enum ClaimEventType {
+    CLAIM_SUBMITTED,
+    CLAIM_APPROVED,
+    CLAIM_REJECTED
+}
---
+++ src/main/java/com/rubin/insurance/policy_management_service/events/PolicyEventType.java
@@ -0,0 +1,7 @@
+package com.rubin.insurance.policy_management_service.events;
+
+public enum PolicyEventType {
+    POLICY_CREATED,
+    POLICY_RENEWED,
+    POLICY_CANCELLED
+}
