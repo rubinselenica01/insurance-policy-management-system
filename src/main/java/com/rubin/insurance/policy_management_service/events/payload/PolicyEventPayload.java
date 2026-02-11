484f46ec0851db850c6e063a865cff59f4048d55
rubinselenica01 <selenicarubin@gmail.com>
2026-02-11 15:56:08 +0100

    Switch PolicyEventPayload to record and add customerEmail.

--- src/main/java/com/rubin/insurance/policy_management_service/events/payload/PolicyEventPayload.java
+++ src/main/java/com/rubin/insurance/policy_management_service/events/payload/PolicyEventPayload.java
@@ -8,24 +8,16 @@ import com.rubin.insurance.policy_management_service.model.policy.PolicyStatus;
 import com.rubin.insurance.policy_management_service.model.policy.PolicyType;
 
 import lombok.Builder;
-import lombok.Value;
 
-@Value
 @Builder
-public class PolicyEventPayload {
-    Long policyId;
-    String policyNumber;
-    PolicyStatus status;
-    PolicyType policyType;
-    LocalDate startDate;
-    LocalDate endDate;
-    BigDecimal coverageAmount;
-    BigDecimal premiumAmount;
-
+public record PolicyEventPayload(Long policyId, String policyNumber, String customerEmail, PolicyStatus status,
+                                 PolicyType policyType, LocalDate startDate, LocalDate endDate,
+                                 BigDecimal coverageAmount, BigDecimal premiumAmount) {
     public static PolicyEventPayload buildPayload(Policy policy) {
         return PolicyEventPayload.builder()
                 .policyId(policy.getId())
                 .policyNumber(policy.getPolicyNumber())
+                .customerEmail(policy.getCustomerEmail())
                 .status(policy.getStatus())
                 .policyType(policy.getPolicyType())
                 .startDate(policy.getStartDate())
