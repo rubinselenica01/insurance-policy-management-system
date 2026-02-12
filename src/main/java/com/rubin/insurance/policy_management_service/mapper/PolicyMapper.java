57e277b08344669e25385e58dd990c56be808a1e
rubinselenica01 <selenicarubin@gmail.com>
2026-02-12 02:19:32 +0100

    Important mapping in mapstruct

--- src/main/java/com/rubin/insurance/policy_management_service/mapper/PolicyMapper.java
+++ src/main/java/com/rubin/insurance/policy_management_service/mapper/PolicyMapper.java
@@ -4,11 +4,13 @@ import com.rubin.insurance.policy_management_service.dto.PolicyRequest;
 import com.rubin.insurance.policy_management_service.dto.PolicyResponse;
 import com.rubin.insurance.policy_management_service.model.policy.Policy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PolicyMapper {
 
     Policy toEntity(PolicyRequest policyRequest);
 
+    @Mapping(source = "status", target = "policyStatus")
     PolicyResponse toDto(Policy policy);
 }
